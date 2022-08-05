72433f54cf0fe8c4596320ffd0bb91655abac821
ValberF <valberfrsantos@gmail.com>
2022-08-05 17:08:18 -0300

    routes created

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,6 +1,13 @@
-module.export = app => {
-    app.get('/', (req, res) => {
-        return res.status(200).send('Backend executando!')
-    })
-    
-}
+module.export = (app) => {
+  app.get("/", (req, res) => {
+    return res.status(200).send("Backend executando!");
+  });
+
+  app.route("/student").get(app.api.student.get).post(app.api.student.post);
+
+  app
+    .route("/student/:id")
+    .get(app.api.student.getById)
+    .delete(app.api.student.delete)
+    .put(app.api.student.put);
+};
